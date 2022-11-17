01f30f25ae9896843ddbe75a2d94a11ef38a2cb5
Marcin Grzebyk <marcin15g@gmail.com>
2022-11-17 14:26:55 +0100

    Moved material imports to separate module

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatToolbarModule
+  ],
+  imports: [],
+  providers: []
+})
+export class MaterialModule { }
