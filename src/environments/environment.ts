624a7bd37fec5f8c37b7312e2a0f2c7ef48ac4f8
Marcin Grzebyk <marcin15g@gmail.com>
2022-11-17 15:11:43 +0100

    Firebase Deployment, Auth and Storage added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'gym-pal-b5c53',
+    appId: '1:32903685562:web:fd86c13948fbbcca946983',
+    storageBucket: 'gym-pal-b5c53.appspot.com',
+    apiKey: 'AIzaSyB74mmRLerTJMb_jo_lx1r1C9EWhAVJ4jU',
+    authDomain: 'gym-pal-b5c53.firebaseapp.com',
+    messagingSenderId: '32903685562',
+  },
   production: false
 };
 
